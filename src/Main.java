8bbb8491a2913d98021fdac6c27b91bae93b7ce8
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2023-01-11 15:42:14 +0100

    Method to print game bord tic tac toe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        String[][] gameBoardTicTacToe = {{" ", "|", " ", "|", " "},
+                                        {"-", "+", "-", "+", "-"},
+                                        {" ", "|", " ", "|", " "},
+                                        {"-", "+", "-", "+", "-"},
+                                        {" ", "|", " ", "|", " "}};
+
+        printGameBoard(gameBoardTicTacToe);
+    }
+
+    private static void printGameBoard(String[][] gameBoard) {
+        for(int i = 0; i < gameBoard.length; i++) {
+            System.out.println();
+            for (int j = 0; j < gameBoard[i].length; j ++) {
+                System.out.print(gameBoard[i][j]);
+            }
+        }
     }
 }

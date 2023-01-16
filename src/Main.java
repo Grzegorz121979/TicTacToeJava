fba7dec7c657ffe3b1ac2637c88a5824fcd5441c
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2023-01-16 17:27:27 +0100

    last commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
     static Scanner keyboard = new Scanner(System.in);
     static Random myRandom = new Random();
+
+    static ArrayList<Integer> playerPosition = new ArrayList<>();
+    static ArrayList<Integer> computerPosition = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -18,16 +18,25 @@ public class Main {
                                         {" ", "|", " ", "|", " "}
                                         };
 
-        System.out.print("Enter your placement (1-9): ");
-
         while (true) {
-            int playerPosition  = keyboard.nextInt();
-            takePosition(gameBoard, playerPosition, "player");
+            System.out.print("Enter your placement (1-9): ");
+            int playerPos  = keyboard.nextInt();
+            while (playerPosition.contains(playerPos) || computerPosition.contains(playerPos)) {
+                System.out.println("Position taken! Enter a correct Position");
+                playerPos = keyboard.nextInt();
+            }
+            takePosition(gameBoard, playerPos, "player");
 
             int compPosition = myRandom.nextInt(9) + 1;
+            while (playerPosition.contains(compPosition) || computerPosition.contains(compPosition)) {
+                compPosition = keyboard.nextInt();
+            }
             takePosition(gameBoard, compPosition, "computer");
 
             printGameBoard(gameBoard);
+
+            String result = winning();
+            System.out.println(result);
         }
     }
 
@@ -46,8 +55,10 @@ public class Main {
 
         if (user.equals("player")) {
             symbol = "X";
+            playerPosition.add(position);
         } else if (user.equals("computer")) {
             symbol = "O";
+            computerPosition.add(position);
         }
 
         switch (position) {
@@ -93,6 +104,28 @@ public class Main {
 
         List crossLeft = Arrays.asList(1, 5, 9);
         List crossRight= Arrays.asList(3, 5, 7);
+
+        List<List> win = new ArrayList<>();
+        win.add(rowTop);
+        win.add(rowMiddle);
+        win.add(rowBottom);
+
+        win.add(columnLeft);
+        win.add(columnMiddle);
+        win.add(columnRight);
+
+        win.add(crossLeft);
+        win.add(crossRight);
+
+        for (List list : win) {
+            if (playerPosition.containsAll(list)) {
+                return "You win!!!";
+            } else if (computerPosition.containsAll(list)) {
+                return "You loose!";
+            } else if (playerPosition.size() + computerPosition.size() == 9) {
+                return "Draw.";
+            }
+        }
 
         return "";
     }
